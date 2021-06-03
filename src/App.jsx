01a1f95789818e95413ef8937e256370077de4ee
Tyler Dietrich <tylerdietrich38@gmail.com>
2021-06-02 21:55:17 -0400

    having trouble with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export function App() {
-  return <div>Hello, World!</div>
+  const [conversion, setConversion] = useState({ rates: [] })
+  const [amount, setAmount] = useState(1)
+
+  useEffect(async () => {
+    const response = await axios.get(
+      `http://api.exchangeratesapi.io/v1/latest?access_key=56a4348392b313cd26250dccbcbb645f`
+    )
+    console.log(response)
+    setConversion(response.data.rates)
+  }, [])
+
+  return (
+    <>
+      <header>
+        <h1>Currency Converter</h1>
+      </header>
+      <main>
+        <section>
+          <input type="number"></input>
+        </section>
+      </main>
+    </>
+  )
 }
