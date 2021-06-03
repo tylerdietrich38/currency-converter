5d0d3b2594a197f333620d2cb994b0601d5e0520
Tyler Dietrich <tylerdietrich38@gmail.com>
2021-06-03 09:35:19 -0400

    want to better understand object.entries and .map

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export function App() {
-  const [conversion, setConversion] = useState({ rates: [] })
+  const [conversion, setConversion] = useState({ rates: {} })
   const [amount, setAmount] = useState(1)
 
   useEffect(async () => {
     const response = await axios.get(
-      `http://api.exchangeratesapi.io/v1/latest?access_key=56a4348392b313cd26250dccbcbb645f`
+      `http://api.exchangeratesapi.io/v1/latest?access_key=139caf47735091d47447d89fa982acf9`
     )
     console.log(response)
     setConversion(response.data.rates)
@@ -20,7 +20,23 @@ export function App() {
       </header>
       <main>
         <section>
-          <input type="number"></input>
+          <ul>
+            <input
+              type="number"
+              value={amount}
+              onChange={(event) => setAmount(event.target.value)}
+            ></input>
+            <button>Convert Currency</button>
+            {Object.entries(conversion.rates).map(
+              ([currencyCode, currencyPrice]) => {
+                return (
+                  <li key={currencyCode}>
+                    {currencyCode}: {(currencyPrice * amount).toFixed(2)}
+                  </li>
+                )
+              }
+            )}
+          </ul>
         </section>
       </main>
     </>
